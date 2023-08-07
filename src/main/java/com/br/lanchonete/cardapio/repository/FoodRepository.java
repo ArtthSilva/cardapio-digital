69e3d866818c7ea65aadf9501ef2771f67941838
ArtthSilva <arthur12pereira1@gmail.com>
2023-08-07 02:23:42 -0300

    feat add: adding the function findByType in FoodRepository

--- src/main/java/com/br/lanchonete/cardapio/repository/FoodRepository.java
+++ src/main/java/com/br/lanchonete/cardapio/repository/FoodRepository.java
@@ -1,12 +1,16 @@
 package com.br.lanchonete.cardapio.repository;
 import com.br.lanchonete.cardapio.entity.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 @Repository
 public interface FoodRepository extends JpaRepository<Food, UUID> {
 
-
+    List<Food> findByType(String type);
+    @Query("SELECT DISTINCT f.type FROM foods f")
+    List<String> findDistinctTypes();
 
 }
